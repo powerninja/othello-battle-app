869d721c7275186a9fdad15a9ac8011e4d56ef5d
powerninja <asdyuy23@gmail.com>
2023-05-05 23:24:10 +0900

    8x8のマス目を表示

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export const App = () => {
-  return <div className="App"></div>;
+  const createGrid = () => {
+    const cells = [];
+    for (let i = 0; i < 64; i++) {
+      cells.push(<div key={i} className="cell"></div>);
+    }
+    return cells;
+  };
+  return (
+    <div className="App">
+      <div className="grid">{createGrid()}</div>
+    </div>
+  );
 };
